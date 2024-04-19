c6aee2960bbff61b57011e60ef5b845e5a3f61b3
Hannahsapre <hannahsapre@gmail.com>
2024-04-20 00:19:27 +0530

    poor js knowledge eh?

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
-import React, { useEffect, useState } from "react";
-import html2canvas from "html2canvas";
-import { saveAs } from "file-saver";
+import { useEffect, useState } from "react";
 import "cropperjs/dist/cropper.css";
 import axios from "axios";
 
 const App = () => {
   const [sourceImage, setSourceImage] = useState(null);
   const [watermarkImage, setWatermarkImage] = useState(null);
+  const [watermarkedImage, setWatermarkedImage] = useState(null);
   const [compressedImage, setCompressedImage] = useState(null);
   const [compressionQuality, setCompressionQuality] = useState(0.8);
   const [startX, setStartX] = useState("");
@@ -79,10 +78,26 @@ const App = () => {
     formData.append("position", "center");
     formData.append("opacity", 0.5);
 
+    let config = {
+      method: "post",
+      maxBodyLength: Infinity,
+      url: "http://3.93.115.39:5001/add_watermark",
+      data: formData,
+      responseType: "arraybuffer",
+    };
+
     axios
-      .post("http://3.93.115.39:5001/add_watermark", formData)
+      .request(config)
       .then((response) => {
-        console.log("Watermark added successfully:", response.data);
+        console.log(response.data);
+        const base64 = btoa(
+          new Uint8Array(response.data).reduce(
+            (data, byte) => data + String.fromCharCode(byte),
+            ""
+          )
+        );
+        const imageDataUrl = `data:image/jpeg;base64,${base64}`;
+        setWatermarkedImage(imageDataUrl);
       })
       .catch((error) => {
         console.error("Error adding watermark:", error);
@@ -101,6 +116,27 @@ const App = () => {
     }
   };
 
+  const handleCDownload = () => {
+    if (compressedImage) {
+      const link = document.createElement("a");
+      link.href = compressedImage;
+      link.download = "compressed_image.jpg";
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+    }
+  };
+  const handleWDownload = () => {
+    if (watermarkedImage) {
+      const link = document.createElement("a");
+      link.href = watermarkedImage;
+      link.download = "watermarked_image.jpg";
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+    }
+  };
+
   const handleCrop = () => {
     let data = new FormData();
     data.append("image", sourceImage);
@@ -110,20 +146,31 @@ const App = () => {
       `[[${startX},${startY}],[${endX},${endY}]]`
     );
 
+    data.append("compression_level", compressionQuality * 100);
+
     let config = {
       method: "post",
       maxBodyLength: Infinity,
-      url: "http://cropper.us-east-1.elasticbeanstalk.com/api/image/crop/",
+      url: "http://cropper.us-east-1.elasticbeanstalk.com/api/image/crop-n-compress/",
       data: data,
+      responseType: "arraybuffer",
     };
 
     axios
       .request(config)
       .then((response) => {
         console.log(response.data);
-        const blob = new Blob([response.data], { type: "image/jpeg" });
-        const blobUrl = URL.createObjectURL(blob);
-        setCompressedImage(blobUrl);
+        const base64 = btoa(
+          new Uint8Array(response.data).reduce(
+            (data, byte) => data + String.fromCharCode(byte),
+            ""
+          )
+        );
+        const imageDataUrl = `data:image/jpeg;base64,${base64}`;
+        setCompressedImage(imageDataUrl);
+        // const blob = new Blob([response.data], { type: "image/jpeg" });
+        // const blobUrl = URL.createObjectURL(blob);
+        // setCompressedImage(blobUrl);
       })
       .catch((error) => {
         console.log(error);
@@ -131,8 +178,9 @@ const App = () => {
   };
 
   return (
-    <div className="max-w-md mx-auto p-4 bg-gray-100 rounded-lg shadow-lg">
+    <div className="p-4 bg-gray-100 rounded-lg shadow-lg">
       <h1 className="text-2xl font-bold mb-4 text-center">Image Processor</h1>
+
       <div className="mb-4">
         <label htmlFor="sourceImageInput" className="block mb-2">
           Select Source Image:
@@ -247,7 +295,7 @@ const App = () => {
           onClick={handleCrop}
           className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600"
         >
-          Crop Image
+          Crop and Compress Image
         </button>
         <button
           onClick={handleWatermark}
@@ -256,17 +304,37 @@ const App = () => {
           Add Watermark
         </button>
       </div>
-      {watermarkImage && (
+      {compressedImage && (
         <div className="mt-4">
           <h2 className="text-xl font-bold mb-2 text-center">
-            Download Watermarked Image:
+            Cropped and Compressed Image:
           </h2>
-          <button
-            onClick={handleDownload}
-            className="bg-green-500 text-white px-4 py-2 rounded-md hover:bg-green-600"
-          >
-            Download
-          </button>
+          <div className=" justify-between">
+            <img src={compressedImage} />
+            <button
+              className="bg-green-500 flex justify-center items-center w-full mt-2 h-10 text-white px-2 py-2 rounded-md hover:bg-green-600"
+              onClick={handleCDownload}
+            >
+              Download Image
+            </button>
+          </div>
+        </div>
+      )}
+      {watermarkedImage && (
+        <div className="mt-4">
+          {/* {console.log(watermarkedImage)} */}
+          <h2 className="text-xl font-bold mb-2 text-center">
+            Watermarked Image:
+          </h2>
+          <div className="justify-between">
+            <img src={watermarkedImage} />
+            <button
+              className="bg-green-500 h-10 text-white px-2 w-full mt-2 py-2 rounded-md hover:bg-green-600"
+              onClick={handleWDownload}
+            >
+              Download Image
+            </button>
+          </div>
         </div>
       )}
     </div>
